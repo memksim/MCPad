34c1353ca7f8d61b8c5dbbacf03e9293ab78f3a6
m.kosenko <m.kosenko@vk.team>
2025-10-19 16:37:34 +0500

    Ask gpt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    id("kotlinx-serialization")
 }
 
 group = "com.memksim"
@@ -15,6 +16,15 @@ dependencies {
     implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.3.0")
     implementation("ai.koog:koog-agents:0.5.1")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+
+    val ktor = "3.3.0"
+    implementation("io.ktor:ktor-client-core:${ktor}")
+    implementation("io.ktor:ktor-client-java:${ktor}")
+    implementation("io.ktor:ktor-client-content-negotiation:${ktor}")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:${ktor}")
+    implementation("io.ktor:ktor-client-logging:${ktor}")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,51 @@
 package com.memksim
 
-fun main() {
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.entities.ChatId
+import io.github.cdimascio.dotenv.dotenv
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
+private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+fun main() {
+    val model = YandexModel(
+        iamToken = dotenv()["YANDEX_IAM_TOKEN"],
+        folderId = dotenv()["FOLDER_ID"],
+    )
+
+    bot {
+        token = dotenv()["TELEGRAM_TOKEN"]
+        dispatch {
+            command("start") {
+                scope.launch {
+                    val r = model.sendRequest()
+                    val result = bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = r.result.alternatives[0].message.text ?: "Не удалось выполнить запрос.")
+                    result.fold(
+                        ifSuccess = { m ->
+                            println("message ${m.text} with id ${m.messageId} was successfully sended")
+                        },
+                        ifError = { e ->
+                            println("error while send message: $e")
+                        }
+                    )
+                }
+            }
+            command("tools") {
+                val result = bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Command /tools was handled")
+                result.fold(
+                    ifSuccess = { m ->
+                        println("message ${m.text} with id ${m.messageId} was successfully sended")
+                    },
+                    ifError = { e ->
+                        println("error while send message: $e")
+                    }
+                )
+            }
+        }
+    }.startPolling()
 }
